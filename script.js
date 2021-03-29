87824ecac3cfc30891075104ae5b7fc367248d0e
pilgr1m <vanjavolokha@gmail.com>
2021-03-29 16:40:53 +0300

    new buttons added

--- script.js
+++ script.js
@@ -3,24 +3,50 @@
 // const numbers = "0123456789";
 // const symbols = "!@#$%^&*()_+?<>:{}[]";
 
-function getPassword() {
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+?<>:{}[]";
-    // всередине функ чтоб обнулся инпут при новом нажатии
-    let password = "";
+const input = document.getElementById("password")
+const copy = document.getElementById("copy")
+const tooltip = document.getElementById("tooltip")
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+?<>:{}[]"
+const timeout = setTimeout(() => tooltip.innerHTML = "Copy to clipboard", 2000)
 
-//икл для 16 повторений - 16-и сгенерированных символов пароля
-for (let i = 0; i < 16; i++) {
-    //рандом число (генерится с помощью длины chars)
-    let randomNumber = Math.floor(Math.random() * chars.length);
-    console.log(randomNumber);
-    // password = password + chars.substring(randomNumber, randomNumber + 1);
-    //метод .substring(инд1,инд2) возращает подстроку с инд1 (включая его) до (конца строки) или до  инд2(необьязательный) (не включая его)
-    password += chars.substring(randomNumber, randomNumber + 1);
+
+function generatePassword() {
+    console.log(input.value === false)
+    let password = ""
+    for (let i = 0; i < 16; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length)
+        console.log(randomNumber)
+        password += chars.substring(randomNumber, randomNumber + 1)
+    }
+    input.value = password
 }
-document.getElementById("password").value = password;
+
+function cleanPassword() {
+    let password = ""
+    input.value = password
+}
+
+// function checkValue() {
+//     if (input.value === "") {
+//         tooltip.innerHTML = "Nothing to copy"
+//         setTimeout(() => tooltip.innerHTML = "Copy to clipboard", 2000)
+//         return
+//     }
+// }
+
+function copyToClipboard() {
+
+    if (input.value === "") {
+        tooltip.innerHTML = "✕ Nothing to copy"
+        setTimeout(() => tooltip.innerHTML = "Copy to clipboard", 2000)
+        return
+    }
+    input.focus()
+    input.select()
+    document.execCommand("copy")
+    tooltip.innerHTML = "✓ Copied"
+    setTimeout(() => tooltip.innerHTML = "Copy to clipboard", 2000)
 }
 
 
-// let one = "123456789";
-// let two = one.substring(3,5);
-// console.log(two);
+
